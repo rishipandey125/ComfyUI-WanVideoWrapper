d7e36da0769c57ec5c9402c071af5695d734c81f
glyfspace <rishipandey125@gmail.com>
2025-05-23 15:46:45 -0700

    updated w/ kijai's code

--- nodes.py
+++ nodes.py
@@ -464,7 +464,7 @@ class WanVideoModelLoader:
                 "model": (folder_paths.get_filename_list("diffusion_models"), {"tooltip": "These models are loaded from the 'ComfyUI/models/diffusion_models' -folder",}),
 
             "base_precision": (["fp32", "bf16", "fp16", "fp16_fast"], {"default": "bf16"}),
-            "quantization": (['disabled', 'fp8_e4m3fn', 'fp8_e4m3fn_fast', 'fp8_e5m2', 'fp8_e4m3fn_fast_no_ffn', 'torchao_fp8dq', "torchao_fp8dqrow", "torchao_int8dq", "torchao_fp6", "torchao_int4", "torchao_int8"], {"default": 'disabled', "tooltip": "optional quantization method"}),
+            "quantization": (['disabled', 'fp8_e4m3fn', 'fp8_e4m3fn_fast', 'fp8_e5m2', 'fp8_e4m3fn_fast_no_ffn'], {"default": 'disabled', "tooltip": "optional quantization method"}),
             "load_device": (["main_device", "offload_device"], {"default": "main_device", "tooltip": "Initial device to load the model to, NOT recommended with the larger models unless you have 48GB+ VRAM"}),
             },
             "optional": {
@@ -1956,6 +1956,8 @@ class WanVideoVACEEncode:
         }
 
         if prev_vace_embeds is not None:
+            if "additional_vace_inputs" in prev_vace_embeds and prev_vace_embeds["additional_vace_inputs"]:
+                vace_input["additional_vace_inputs"] = prev_vace_embeds["additional_vace_inputs"].copy()
             vace_input["additional_vace_inputs"].append(prev_vace_embeds)
     
         return (vace_input,)
@@ -2419,6 +2421,8 @@ class WanVideoSampler:
             has_ref = image_embeds.get("has_ref", False)
             vace_context = image_embeds.get("vace_context", None)
             vace_scale = image_embeds.get("vace_scale", None)
+            if not isinstance(vace_scale, list):
+                vace_scale = [vace_scale] * (steps+1)
             vace_start_percent = image_embeds.get("vace_start_percent", 0.0)
             vace_end_percent = image_embeds.get("vace_end_percent", 1.0)
             vace_seqlen = image_embeds.get("vace_seq_len", None)
@@ -2437,9 +2441,12 @@ class WanVideoSampler:
                     for i in range(len(vace_additional_embeds)):
                         if vace_additional_embeds[i].get("has_ref", False):
                             has_ref = True
+                        vace_scale = vace_additional_embeds[i]["vace_scale"]
+                        if not isinstance(vace_scale, list):
+                            vace_scale = [vace_scale] * (steps+1)
                         vace_data.append({
                             "context": vace_additional_embeds[i]["vace_context"],
-                            "scale": vace_additional_embeds[i]["vace_scale"],
+                            "scale": vace_scale,
                             "start": vace_additional_embeds[i]["vace_start_percent"],
                             "end": vace_additional_embeds[i]["vace_end_percent"],
                             "seq_len": vace_additional_embeds[i]["vace_seq_len"]
